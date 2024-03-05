2c665a163f7042e8ea1485917779658aca11de26
pankaj suthar <pankajss0070@gmail.com>
2024-03-05 23:08:25 +0530

    event page

---
+++ src/app/book-event/[eventid]/page.tsx
@@ -0,0 +1,66 @@
+import { EventType } from "@/interfaces/events";
+import EventModel from "@/models/event-model";
+import React from "react";
+
+interface Props {
+  params: {
+    eventid: string;
+  };
+}
+
+const BookEventPage = async ({ params }: Props) => {
+  const event: EventType = (await EventModel.findById(params.eventid)) as any;
+
+  const getEventProperty = (property: string) => {
+    return (
+      <div className="flex flex-col text-sm">
+        <h1 className="font-semibold capitalize">{property}</h1>
+        <h1 className="text-gray-600">{event[property as keyof EventType]}</h1>
+      </div>
+    );
+  };
+
+  return (
+    <div className="bg-white">
+      <div className="bg-gray-700 p-5 text-white flex flex-col gap-3">
+        <h1 className="text-3xl font-semibold">{event.name}</h1>
+        <div className="text-sm text-gray-200 flex gap-10">
+          <h1>
+            <i className="ri-map-pin-line pr-2"></i> {event.location}
+          </h1>
+          <h1>
+            <i className="ri-calendar-line pr-2"></i> {event.date} at{" "}
+            {event.time}
+          </h1>
+        </div>
+      </div>
+      <div className="p-5">
+        <div className="flex gap-5 flex-wrap overflow-x-auto mt-7">
+          {event.images.map((image) => (
+            <img
+              src={image}
+              alt="image of event"
+              height={280}
+              width={400}
+              className="rounded-sm"
+            />
+          ))}
+        </div>
+        <p className="text-gray-500 w-full mt-7 text-sm">{event.description}</p>
+        <div className="mt-5 bg-gray-100 border border-gray-200 p-3 rounded-sm grid grid-cols-3 gap-5">
+          {getEventProperty("organizer")}
+          {getEventProperty("location")}
+          {getEventProperty("date")}
+          {getEventProperty("time")}
+
+          <div className="flex flex-col text-sm col-span-2">
+            <h1 className="font-semibold capitalize">Chief Guests</h1>
+            <h1 className="text-gray-600">{event.guests.join(", ")}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookEventPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,59 @@
-import {
-  getMongoDBUserIdOfLoggedInUser,
-  handleNewUserRegistration,
-} from "@/actions/users";
+import { handleNewUserRegistration } from "@/actions/users";
 import { connectDB } from "@/config/dbConfig";
-import { UserButton } from "@clerk/nextjs";
+import { EventType } from "@/interfaces/events";
+import EventModel from "@/models/event-model";
+import { Link } from "@nextui-org/react";
 connectDB();
 
 export default async function Home() {
   await handleNewUserRegistration();
 
-  const mongoUserId = await getMongoDBUserIdOfLoggedInUser();
-  console.log("mongodb id :", mongoUserId);
-  return <div>Homepage</div>;
+  const events: EventType[] = (await EventModel.find().sort({
+    createdAt: -1,
+  })) as any;
+  return (
+    <div>
+      <div className="flex flex-col gap-5">
+        {events.map((event) => (
+          <div
+            key={event._id}
+            className="grid grid-cols-3 bg-white rounded-md gap-10 border border-gray-200"
+          >
+            <div className="col-span-1">
+              <img
+                src={event.images[0]}
+                alt="image of event"
+                height={130}
+                width={250}
+                className="w-full object-contain rounded-sm"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col gap-5 justify-between p-5">
+              <h1 className="semibold text-gray-800">{event.name}</h1>
+              <p className="text-gray-500 w-full line-clamp-3 text-sm">
+                {event.description}
+              </p>
+              <div className="flex justify-between items-center">
+                <div className="text-sm">
+                  <h1>
+                    <i className="ri-map-pin-line pr-5"></i> {event.location}
+                  </h1>
+                  <h1>
+                    <i className="ri-calendar-line pr-5"></i> {event.date} at{" "}
+                    {event.time}
+                  </h1>
+                </div>
+                <Link
+                  href={`/book-event/${event._id}`}
+                  className="bg-primary text-white px-5 py-2 rounded-sm text-sm"
+                >
+                  View Event
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
